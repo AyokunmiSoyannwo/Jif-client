49b57d8b40ad6ce3ee0f03ea84a5cab88257f93a
Matthew Lo <matthewlol316@gmail.com>
2022-08-30 12:16:24 +0100

    add 7 test to layout

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,5 @@
 /**
- * @jest-environnment jsdom
+ * @jest-environment jsdom
  */
 
 const fs = require("fs");
@@ -11,12 +11,47 @@ describe("webpage", () => {
         document.documentElement.innerHTML = html.toString();  //beforeEach can be used if an action needs to be repeated before each test
     })
 
-    describe("main page", () => {  //usually the first test is to that the object exists, i can use expect(object).toBeTruthy(); to do this
+    describe("Nav bar", () => {  //usually the first test is to that the object exists, i can use expect(object).toBeTruthy(); to do this
 
         test("it has a navbar", () => {
             let navbar = document.querySelector("nav")
             expect(navbar).toBeTruthy()
         })
 
+        test("it has a searchbar", () => {
+            let serachbar = document.querySelector("searchbar")
+            expect(searchbar).toBeTruthy()
+        })
+
+        test("it has navButtons class", () => {
+            let navButtons = document.getElementsByClassName("navButton")
+            expect(navButtons).toBeTruthy()
+        })
+
+    })
+
+    describe('Card Holder', () => {
+        test("it has a card holder", () => {
+            let cardHolder = document.getElementsByClassName("cardHolder")
+            expect(cardHolder).toBeTruthy()
+        })
+    })
+
+    describe('Individual Card', () => {
+        test("it has a card div", () => {
+            let cardDiv = document.getElementsByClassName("card")
+            expect(cardDiv).toBeTruthy()
+        })
+
+        test("it has a H2 title", () => {
+            let h2 = document.getElementsByTagName("h2")[1]
+            expect(h2.innerHTML).toBe('title')
+        })
+
+        test("it has a paragraph", () => {
+            let cardText = document.getElementsByClassName("cardText")[1]
+            console.log(cardText.innerHTML)
+            expect(cardText.innerHTML).toBeTruthy
+        })
     })
 })
