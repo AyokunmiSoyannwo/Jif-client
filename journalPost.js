808657cdd1f4198089030f88373ea9a9dddc1d78
Matthew Lo <matthewlol316@gmail.com>
2022-08-31 14:16:00 +0100

    add serach gif and styling

--- journalPost.js
+++ journalPost.js
@@ -25,7 +25,7 @@ function postEntry(e){
 
     const journalData = {
         title: e.target.titleInput.value,
-        text: e.target.textInput.value
+        text: e.target.textInput.value,
     };
     console.log(journalData)
     const options = { 
@@ -58,3 +58,58 @@ textarea.addEventListener("input", () => {
     p.textContent = `${messageLength}/300`
     
 })
+
+
+// --------------------- SERACH GIF ------------------//
+function getUserInput() {
+    var inputValue = document.querySelector('.js-userinput').value;
+    return inputValue;
+}
+
+document.querySelector('.search').addEventListener('click', function () {
+    var inputValue = document.querySelector('.js-userinput').value;
+    var userInput = getUserInput();
+    searchGiphy(userInput)
+})
+
+document.querySelector(".js-userinput").addEventListener("keyup", e => {
+    if (e.which === 13){
+        var userInput = getUserInput();
+        searchGiphy(userInput);
+    }
+})
+
+function searchGiphy (searchQuery){
+    fetch (`https://api.giphy.com/v1/gifs/search?api_key=df1eFJpWqUNTLxpPJKHU82efGhRLon6J&q=${searchQuery}&limit=25&offset=0&rating=g&lang=en`)
+    .then(response => response.json())
+    .then(displayResults)
+}
+
+function displayResults(response){
+    var img = response.data;
+    var container = document.querySelector(".js-container");
+    container.innerHTML = "";
+    img.forEach(function (image) {
+        var src = image.images.fixed_height.url;
+        container.innerHTML += "<img src='" 
+          + src + "' class='container-image' />";
+        container.addEventListener('click', displaySelection)
+      });      
+}
+
+
+function displaySelection(e){
+    selectImg = document.querySelector(".container-image")
+    selectImg.addEventListener('click', e => {
+        selection = e.getAttribute('src')
+        console.log(selection)
+        
+    })
+    console.log("I'm clicked")
+    var selection = document.querySelector('#selection');
+    selection.src = e.target.src
+    var gif = document.querySelector('#gif');
+    gif.inputValue = e.target.src
+    console.log(gif.inputValue)
+}
+
